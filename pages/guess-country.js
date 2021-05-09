f3a4e3abfbc337b506b4fb8c8a55e38644bd6e35
Poonam Thorup <poonam.thorup@gmail.com>
2021-05-10 00:27:32 +0200

    added lives and game over

--- pages/guess-country.js
+++ pages/guess-country.js
@@ -27,6 +27,9 @@ import { useEffect, useState } from 'react'
 const GuessCountry = ({ countries }) => {
    const [answerCountryLetters, setAnswerCountryLetters] = useState([])
    const [guessedLetters, setGuessedLetters] = useState([])
+   const maxLives = 5
+   const [userLives, setUserLives] = useState(maxLives)
+
    let isGameOver =
       guessedLetters.length === 0 || guessedLetters.includes('_') ? false : true
 
@@ -64,7 +67,7 @@ const GuessCountry = ({ countries }) => {
       // const randomNum = Math.floor(Math.random() * countries.length)
       // let countryRandom = [...countries[randomNum].name]
 
-      let countryRandom = ['A', 'a', 'c', ' ', 'e']
+      let countryRandom = ['A', 'a', 'c', ' ', 'e', 'f', 'w']
 
       // Make an array to hold hidden letters
       const countryRandomDashes = []
@@ -78,6 +81,7 @@ const GuessCountry = ({ countries }) => {
       // Set answer and user's guessed letters to respective states
       setAnswerCountryLetters(countryRandom)
       setGuessedLetters(countryRandomDashes)
+      setUserLives(maxLives)
    }
 
    useEffect(() => {
@@ -90,13 +94,16 @@ const GuessCountry = ({ countries }) => {
    const handleLetterClick = (e) => {
       let userChosenLetter = e.target.textContent
       const updateGuessLetters = [...guessedLetters]
+      let isCorrectLetter = false
 
       for (let i = 0; i < answerCountryLetters.length; i++) {
-         answerCountryLetters[i].toLowerCase() === userChosenLetter
-            ? (updateGuessLetters[i] = answerCountryLetters[i])
-            : ''
+         if (answerCountryLetters[i].toLowerCase() === userChosenLetter) {
+            updateGuessLetters[i] = answerCountryLetters[i]
+            isCorrectLetter = true
+         }
       }
       setGuessedLetters(updateGuessLetters)
+      !isCorrectLetter ? setUserLives((prev) => prev - 1) : ''
    }
 
    return (
@@ -105,7 +112,12 @@ const GuessCountry = ({ countries }) => {
          <div style={{ display: 'flex' }}>
             {alphabet.map((letter) => (
                <div key={letter}>
-                  <button onClick={handleLetterClick}>{letter}</button>
+                  <button
+                     onClick={handleLetterClick}
+                     disabled={isGameOver || userLives === 0}
+                  >
+                     {letter}
+                  </button>
                </div>
             ))}
          </div>
@@ -116,7 +128,15 @@ const GuessCountry = ({ countries }) => {
                </div>
             ))}
          </div>
-         {isGameOver ? (
+         <div style={{ color: 'green', fontSize: '40px' }}>{userLives}</div>
+
+         {userLives === 0 ? (
+            <div>
+               <div>Sorry, you ran out of lives :(</div>
+               <div>Correct answer is: {answerCountryLetters.join('')}</div>
+               <button onClick={getRandomCountry}>Play again</button>
+            </div>
+         ) : isGameOver ? (
             <div>
                <div>You won!!!</div>
                <button onClick={getRandomCountry}>Play again</button>
