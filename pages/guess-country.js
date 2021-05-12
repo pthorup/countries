57142ad8111cf2b99ef6e9031ff5bda81b984730
Poonam Thorup <poonam.thorup@gmail.com>
2021-05-13 00:27:32 +0200

    fixed isGameOver

--- pages/guess-country.js
+++ pages/guess-country.js
@@ -1,33 +1,33 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 import { useEffect, useState } from 'react'
 
-// export const getStaticProps = async () => {
-//    const client = new ApolloClient({
-//       uri: 'https://countries.trevorblades.com/',
-//       cache: new InMemoryCache(),
-//    })
+export const getStaticProps = async () => {
+   const client = new ApolloClient({
+      uri: 'https://countries.trevorblades.com/',
+      cache: new InMemoryCache(),
+   })
 
-//    const { data } = await client.query({
-//       query: gql`
-//          {
-//             countries {
-//                name
-//             }
-//          }
-//       `,
-//    })
+   const { data } = await client.query({
+      query: gql`
+         {
+            countries {
+               name
+            }
+         }
+      `,
+   })
 
-//    return {
-//       props: {
-//          countries: data.countries,
-//       },
-//    }
-// }
+   return {
+      props: {
+         countries: data.countries,
+      },
+   }
+}
 
 const GuessCountry = ({ countries }) => {
    const [answerCountryLetters, setAnswerCountryLetters] = useState([])
    const [guessedLetters, setGuessedLetters] = useState([])
-   const isGameOver = guessedLetters.includes('_') ? false : true
+   const isGameOver = false
    const alphabet = [
       'a',
       'b',
@@ -59,9 +59,8 @@ const GuessCountry = ({ countries }) => {
 
    const getRandomCountry = () => {
       // Get Random Country
-      // const randomNum = Math.floor(Math.random() * countries.length)
-      // let countryRandom = [...countries[randomNum].name]
-      let countryRandom = ['a', 'A', ' ', 'C', '.']
+      const randomNum = Math.floor(Math.random() * countries.length)
+      let countryRandom = [...countries[randomNum].name]
 
       // Make an array to hold hidden letters
       const countryRandomDashes = []
@@ -94,6 +93,7 @@ const GuessCountry = ({ countries }) => {
             : ''
       }
       setGuessedLetters(updateGuessLetters)
+      isGameOver = guessedLetters.includes('_') ? false : true
    }
 
    return (
