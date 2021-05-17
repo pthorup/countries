46de6d2d7df46fcca44bcd6776726d3219a78471
Poonam Thorup <poonam.thorup@gmail.com>
2021-05-17 10:16:51 +0200

    minor changes

--- test/Menu.Snapshots.test.js
+++ test/Menu.Snapshots.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Menu from '../components/Menu'
 import renderer from 'react-test-renderer'
-import { answer } from '../mockData'
 
 it('Menu links', () => {
    const tree = renderer.create(<Menu />)
